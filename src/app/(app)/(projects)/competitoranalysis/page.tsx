664978ff8e93282900b4c6e5248e9a7c4e4181f5
vihu360 <vivekbarnwal360@gmail.com>
2024-09-26 11:26:55 +0530

    deployment fix

---
+++ src/app/(app)/(projects)/competitoranalysis/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+	return (
+		<div className='w-full h-full flex justify-center items-center'>
+
+			This page is under construction.Please look out for other features
+
+		</div>
+	)
+}
+
+export default page
